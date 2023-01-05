0084a9f02de798d200a8afd5a8dbcaa94cff13ba
solra5653 <121776248+solra5653@users.noreply.github.com>
2023-01-05 09:34:52 +0900

    java 정리

--- oopProject/src/oop/access/FinalSample.java
+++
@@ -1,9 +0,0 @@
-package oop.access;
-
-//종단(끝) 클래스라고 함
-//상속에서 부모가 될 수 없는 클래스임(후손을 만들 수 없음)
-//클래스의 기능 확장을 막고자 할때 사용함
-
-public final class FinalSample {
-
-}
--- oopProject/src/oop/access/PublicSample.java
+++
@@ -1,8 +0,0 @@
-package oop.access;
-
-//일반적인 기능 제공용 클래스는 public  클래스임
-//접근범위 : 패키지 안/밖 어디서나 사용 가능
-//패키지 밖에서는 import 선언 하고 사용 가능함.
-public class PublicSample {
-
-}
